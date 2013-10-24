223967fb7bf5277b48bdb6e9404bf106ed19c6c6
Eric Kern <ekern@mitre.org>
2013-10-24 12:55:43 -0400

    Modified make_links.py to have explicit files to link

--- make_links.py
+++ make_links.py
@@ -1,43 +1,41 @@
-def create_link(file_path, link_path):
-	success = True
+LINKS = {
+        'colorgcc/colorgccrc' : '.colorgccrc',
+        'git/gitconfig' : '.gitconfig',
+        'tmux/tmux.conf' : '.tmux.conf',
+        'vim/vim' : '.vim',
+        'vim/vimrc' : '.vimrc',
+        'zsh/oh-my-zsh' : '.oh-my-zsh',
+        'zsh/zshrc' : '.zshrc'
+}
 
-	try:
-		os.symlink(file_path, link_path)
-	except OSError:
-		success = False
-	
-	return success
+def create_link(file_path, link_path):
+    print file_path, link_path
+    if os.path.exists(link_path):
+        answer = raw_input('Override path %s? (y/N)' % link_path)
+
+        if not answer in ['y', 'Y', 'yes', 'Yes']:
+            return False
+
+    success = True
+    try:
+        os.symlink(file_path, link_path)
+    except OSError:
+        success = False
+    
+    return success
+
+def create_all_links(base_dir):
+    for source, dest in LINKS.iteritems():
+        print 'Linking %s --> %s' % (source, dest)
+        if not create_link(base_dir + source, os.path.expanduser('~/' + dest)):
+            print 'Error'
 
 if __name__ == '__main__':
-	import sys, os
+    import sys, os
 
-	if os.path.isabs(sys.argv[0]):
-		config_dir = os.path.dirname(sys.argv[0])
-	else:
-		config_dir = os.getcwd() + '/' + os.path.dirname(sys.argv[0])
-	
-	for d in os.listdir(config_dir):
-		dir_path = config_dir + '/' + d
-
-		if not os.path.isdir(dir_path):
-			continue
-
-		if d[0] == '.':
-			continue
-
-		for f in os.listdir(dir_path):
-			file_path = dir_path + '/' + f
-			link_path = os.path.expanduser('~/.' + f)
-			print 'Linking', file_path, 'to', link_path
-
-
-			if not create_link(file_path, link_path):
-				print 'File', f, 'already exists. Replace? (Y/n)',
-				ch = raw_input()
-
-				if ch == 'n' or ch == 'N':
-					print 'Skipping', f
-				else:
-					os.remove(link_path)
-					create_link(file_path, link_path)
-
+    if os.path.isabs(sys.argv[0]):
+        config_dir = os.path.dirname(sys.argv[0])
+    else:
+        config_dir = os.getcwd() + '/' + os.path.dirname(sys.argv[0])
+    
+    create_all_links(config_dir)

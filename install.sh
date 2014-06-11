22a1b973e613a9f2c3694c117bb8b24f59f5ccd5
Eric Kern <ekern@mitre.org>
2014-06-11 15:34:32 -0400

    Start of reorg to use Vundle

--- install.sh
+++ install.sh
@@ -123,60 +123,11 @@ command -v zsh >/dev/null 2>&1 || {
 	rm -rf zsh-5.0.2
 }
 
-# Download, build and install clang
-
-if [ ! -d llvm ]; then
-	git clone http://llvm.org/git/llvm.git
-
-	if [ $? -ne 0 ]; then
-		echo "Failed to download LLVM"
-		exit 1
-	fi
-fi
-
-if [ ! -d llvm/tools/clang ]; then
-	cd llvm/tools
-	git clone http://llvm.org/git/clang.git
-
-	if [ $? -ne 0 ]; then
-		echo "Failed to download clang"
-		exit 1
-	fi
-	
-	cd $base_dir
-fi
-
-if [ ! -d llvm/projects/compiler-rt ]; then
-	cd llvm/projects
-	git clone http://llvm.org/git/compiler-rt.git
-
-	if [ $? -ne 0 ]; then
-		echo "Failed to download compiler-rt"
-		exit 1
-	fi
-
-	cd base_dir
-fi
-
 cd $base_dir
 
-if [ ! -e llvm-build/Release+Asserts/bin/clang-format ] || [ ! -e llvm-build/Release+Asserts/lib/libclang.so ]; then
-	mkdir llvm-build
-	cd llvm-build
-	../llvm/configure --enable-optimized
+apt-get install clang-format
 
-	if [ $? -ne 0 ]; then
-		echo "Failed to configure LLVM"
-		exit 1
-	fi
-
-	make
-
-	if [ $? -ne 0 ]; then
-		echo "Failed to build LLVM"
-		exit 1
-	fi
-fi
+vim +PluginInstall +qall
 
 cd $base_dir
 cd vim/vim/bundle/YouCompleteMe
@@ -187,20 +138,11 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-mkdir ycm-build
-cd ycm-build
-
-cmake -G "Unix Makefiles" -DEXTERNAL_LIBCLANG_PATH=$base_dir/llvm-build/Release+Asserts/lib/libclang.so . ../cpp
+./install --clang-completer
 
 if [ $? -ne 0 ]; then
-	echo "YouCompleteMe cmake failed"
+	echo "YouCompleteMe make failed"
 	exit 1
-fi
-
-make ycm_support_libs
-
-if [ $? -ne 0 ]; then
-	echo "YouCompleteMe build failed"
 fi
 
 cd $base_dir

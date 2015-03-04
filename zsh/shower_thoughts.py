85a06433834751115612da80792454dfdb8b14c6
Eric Kern <ekern@mitre.org>
2015-03-04 09:55:30 -0500

    Changed user agent of shower thoughts scripts

--- zsh/shower_thoughts.py
+++ zsh/shower_thoughts.py
@@ -4,9 +4,11 @@ import sys
 import urllib2
 
 REDDIT_URL = 'http://reddit.com/r/showerthoughts/hot.json?limit=1'
+USER_AGENT = 'linux:shower_thoughts_script:v1.0.0'
 
 def main():
-    json_string = urllib2.urlopen(REDDIT_URL).read()
+    request = urllib2.Request(REDDIT_URL, headers = {'User-Agent' : USER_AGENT})
+    json_string = urllib2.urlopen(request).read()
     print json.loads(json_string)['data']['children'][0]['data']['title']
 
 if __name__ == '__main__':

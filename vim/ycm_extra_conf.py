22f7cdef009913093ad7caed6e63db34a6b57180
Eric Kedaigle <ekedaigle@mitre.org>
2016-11-15 17:02:00 -0500

    Updated ycm_extra_conf to support ninja deps files

--- vim/ycm_extra_conf.py
+++ vim/ycm_extra_conf.py
@@ -1,53 +1,60 @@
-# This file is NOT licensed under the GPLv3, which is the license for the rest
-# of YouCompleteMe.
-# # Here's the license text for this file:
+#!/usr/bin/env python
 #
-# This is free and unencumbered software released into the public domain.
+# Copyright (C) 2014  Google Inc.
 #
-# Anyone is free to copy, modify, publish, use, compile, sell, or
-# distribute this software, either in source code form or as a compiled
-# binary, for any purpose, commercial or non-commercial, and by any
-# means.
+# This file is part of YouCompleteMe.
 #
-# In jurisdictions that recognize copyright laws, the author or authors
-# of this software dedicate any and all copyright interest in the
-# software to the public domain. We make this dedication for the benefit
-# of the public at large and to the detriment of our heirs and
-# successors. We intend this dedication to be an overt act of
-# relinquishment in perpetuity of all present and future rights to this
-# software under copyright law.
+# YouCompleteMe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
+# YouCompleteMe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# For more information, please refer to <http://unlicense.org/>
+# You should have received a copy of the GNU General Public License
+# along with YouCompleteMe.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import struct
 import ycm_core
 
-path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
+# These are the compilation flags that will be used in case there's no
+# compilation database set (by default, one is not set).
+# CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
+flags = [
+'-Wall',
+'-Wextra',
+'-DNDEBUG',
+'-std=c++11',
+'-x',
+'c++',
+'-isystem',
+'/usr/include',
+'-isystem',
+'/usr/local/include'
+]
 
-ninja_file = '../cmake-build/build.ninja'
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
 # compile_commands.json file to use that instead of 'flags'. See here for
 # more details: http://clang.llvm.org/docs/JSONCompilationDatabase.html
 #
 # Most projects will NOT need to set this to anything; you can just change the
-# 'flags' list of compilation flags. Notice that YCM itself uses that approach.
-compilation_database_folder = ''
+# 'flags' list of compilation flags.
+compilation_database_folder = os.path.abspath('build')
 
 if os.path.exists( compilation_database_folder ):
   database = ycm_core.CompilationDatabase( compilation_database_folder )
 else:
   database = None
 
-SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
+CXX_EXTENSIONS = ('.C', '.cc', '.cpp', '.CPP', '.c++', '.cp', '.cxx')
+OBJC_EXTENSIONS = ('.m', '.mm')
+C_EXTENSIONS = ('.c',)
+SOURCE_EXTENSIONS = CXX_EXTENSIONS + OBJC_EXTENSIONS + C_EXTENSIONS
 
 def DirectoryOfThisScript():
   return os.path.dirname( os.path.abspath( __file__ ) )
@@ -58,6 +65,7 @@ def MakeRelativePathsInFlagsAbsolute( flags, working_directory ):
     return list( flags )
   new_flags = []
   make_next_absolute = False
+  path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
   for flag in flags:
     new_flag = flag
 
@@ -85,6 +93,73 @@ def IsHeaderFile( filename ):
   extension = os.path.splitext( filename )[ 1 ]
   return extension in [ '.h', '.hxx', '.hpp', '.hh' ]
 
+def IsSourceFile( filename ):
+  extension = os.path.splitext( filename )[ 1 ]
+  return extension in SOURCE_EXTENSIONS
+
+def FindSourceFile( text ):
+    for filename in (t.strip(' \\') for t in text.splitlines()):
+        if filename.endswith(SOURCE_EXTENSIONS):
+            return filename
+
+    return None
+
+def FindHeaderFileSourceNinja(filename, f):
+    filename = os.path.relpath(filename, compilation_database_folder)
+    if f.read(12) != '# ninjadeps\n':
+        return None
+
+    f.read(4) # ignore version
+
+    paths = []
+
+    while True:
+        length_s = f.read(4)
+
+        if length_s == '':
+            break
+
+        length = struct.unpack('I', length_s)[0]
+
+        if length < 0x80000000: # path record
+            name = f.read(length - 4).rstrip('\x00')
+            paths.append(name)
+            f.read(4) # ignore
+        else: # dependency record
+            length -= 0x80000000
+            out_id = struct.unpack('I', f.read(4))[0]
+            f.read(4) # ignore time
+            length -= 8
+            in_ids = struct.unpack('I' * (length / 4), f.read(length))
+
+            if filename in [paths[in_id] for in_id in in_ids]:
+                c_files = [paths[in_id] for in_id in in_ids if IsSourceFile(paths[in_id])]
+
+                if len(c_files) > 0:
+                    return os.path.normpath(os.path.join(compilation_database_folder, c_files[0]))
+
+def FindHeaderFileSource( filename ):
+    # first try to find a ninja deps file
+    try:
+        with open(os.path.join(compilation_database_folder, '.ninja_deps')) as f:
+            r = FindHeaderFileSourceNinja(filename, f)
+
+            if not r is None:
+                return r
+    except IOError:
+        pass
+
+    with open(os.path.join(compilation_database_folder, 'CMakeFiles', 'TargetDirectories.txt'), 'r') as targets:
+        for line in targets:
+            for root, dirs, files in os.walk(line.strip()):
+                for f in files:
+                    if f.endswith('.d'):
+                        with open(os.path.join(root, f)) as dep_file:
+                            text = dep_file.read()
+                            if filename in text:
+                                return FindSourceFile(text)
+
+    return None
 
 def GetCompilationInfoForFile( filename ):
   # The compilation_commands.json file generated by CMake does not have entries
@@ -92,80 +167,41 @@ def GetCompilationInfoForFile( filename ):
   # corresponding source file, if any. If one exists, the flags for that file
   # should be good enough.
   if IsHeaderFile( filename ):
-    basename = os.path.splitext( filename )[ 0 ]
-    for extension in SOURCE_EXTENSIONS:
-      replacement_file = basename + extension
-      if os.path.exists( replacement_file ):
-        compilation_info = database.GetCompilationInfoForFile(
-          replacement_file )
+    replacement = FindHeaderFileSource(filename)
+    if replacement is not None and os.path.exists(replacement):
+        compilation_info = database.GetCompilationInfoForFile(replacement)
+
         if compilation_info.compiler_flags_:
-          return compilation_info
+            if replacement.endswith(CXX_EXTENSIONS):
+                compilation_info.compiler_flags_.append('-x')
+                compilation_info.compiler_flags_.append('c++')
+
+            return compilation_info
+
     return None
+
   return database.GetCompilationInfoForFile( filename )
 
-def fix_path(flag, build_dir):
-    for path_flag in path_flags:
-        if flag.startswith(path_flag):
-            directory = flag[len(path_flag):]
-            return path_flag + build_dir + '/' + directory
 
-    return flag
-
+# This is the entry point; this function is called by ycmd to produce flags for
+# a file.
 def FlagsForFile( filename, **kwargs ):
   if database:
     # Bear in mind that compilation_info.compiler_flags_ does NOT return a
     # python list, but a "list-like" StringVec object
     compilation_info = GetCompilationInfoForFile( filename )
     if not compilation_info:
-      return None
+        return { 'flags' : flags, 'do_cache' : True }
 
     final_flags = MakeRelativePathsInFlagsAbsolute(
       compilation_info.compiler_flags_,
       compilation_info.compiler_working_dir_ )
-
   else:
-    cwd = DirectoryOfThisScript()
-    ninja_file_abs = os.path.join(cwd, ninja_file)
-    build_dir_abs = os.path.dirname(ninja_file_abs)
+    relative_to = DirectoryOfThisScript()
+    final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
 
-    with open(os.path.join(cwd, ninja_file), 'r') as f:
-        lines = iter(f)
-        line = lines.next()
+  return {
+    'flags': final_flags,
+    'do_cache': True
+  }
 
-        try:
-            while True:
-                while not line.startswith('build '):
-                    line = lines.next()
-
-                colon = line.find(':')
-                bar = line.find('|')
-
-                if bar > colon:
-                    line = line[colon+1:bar]
-                else:
-                    line = line[colon+1:]
-
-                files = [os.path.basename(f) for f in line.split()[1:]]
-
-                if os.path.basename(filename) in files:
-                    flags = []
-                    
-                    line = lines.next()
-                    while not line.startswith('build '):
-                        if line.strip().startswith('FLAGS') or line.strip().startswith('DEFINES'):
-                            flags.extend(line.partition('=')[2].split())
-                        line = lines.next()
-
-                    #flags = [fix_path(flag, os.path.dirname(ninja_file)) for flag in flags]
-                    flags = MakeRelativePathsInFlagsAbsolute(flags, build_dir_abs)
-                    flags = [flag.replace('-I', '-isystem', 1)
-                            if flag.startswith('-I') else flag for flag in flags]
-
-                    return {
-                      'flags': flags,
-                      'do_cache': True
-                    }
-        except StopIteration:
-            pass
-    
-    raise ValueError('Could not find file ' + filename)

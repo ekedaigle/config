7e6c912ca32fbb1f2eda91aab71279573ea487a1
Eric Kedaigle <ekedaigle@mitre.org>
2017-05-22 17:54:21 -0400

    Updated formatting tools, and added some vim session support

--- vim/clang-format.py
+++ vim/clang-format.py
@@ -2,8 +2,8 @@
 # - Change 'binary' if clang-format is not on the path (see below).
 # - Add to your .vimrc:
 #
-#   map <C-I> :pyf <path-to-this-file>/clang-format.py<CR>
-#   imap <C-I> <ESC>:pyf <path-to-this-file>/clang-format.py<CR>i
+#   map <C-I> :pyf <path-to-this-file>/clang-format.py<cr>
+#   imap <C-I> <c-o>:pyf <path-to-this-file>/clang-format.py<cr>
 #
 # The first line enables clang-format for NORMAL and VISUAL mode, the second
 # line adds support for INSERT mode. Change "C-I" to another binding if you
@@ -14,52 +14,103 @@
 # VISUAL mode. The line or region is extended to the next bigger syntactic
 # entity.
 #
+# You can also pass in the variable "l:lines" to choose the range for
+# formatting. This variable can either contain "<start line>:<end line>" or
+# "all" to format the full file. So, to format the full file, write a function
+# like:
+# :function FormatFile()
+# :  let l:lines="all"
+# :  pyf <path-to-this-file>/clang-format.py
+# :endfunction
+#
 # It operates on the current, potentially unsaved buffer and does not create
 # or save any files. To revert a formatting, just undo.
+from __future__ import print_function
 
-import vim
+import difflib
+import json
+import platform
 import subprocess
+import sys
+import vim
 
+# set g:clang_format_path to the path to clang-format if it is not on the path
 # Change this to the full path if clang-format is not on the path.
 binary = 'clang-format'
+if vim.eval('exists("g:clang_format_path")') == "1":
+  binary = vim.eval('g:clang_format_path')
 
-# Change this to format according to other formatting styles (see 
-# clang-format -help)
+# Change this to format according to other formatting styles. See the output of
+# 'clang-format --help' for a list of supported styles. The default looks for
+# a '.clang-format' or '_clang-format' file to indicate the style that should be
+# used.
 style = 'file'
+fallback_style = None
+if vim.eval('exists("g:clang_format_fallback_style")') == "1":
+  fallback_style = vim.eval('g:clang_format_fallback_style')
 
-# Get the current text.
-buf = vim.current.buffer
-text = "\n".join(buf)
+def get_buffer(encoding):
+  if platform.python_version_tuple()[0] == '3':
+    return vim.current.buffer
+  return [ line.decode(encoding) for line in vim.current.buffer ]
 
-# Determine range to format.
-offset = int(vim.eval('line2byte(' +
-                      str(vim.current.range.start + 1) + ')')) - 1
-length = int(vim.eval('line2byte(' +
-                      str(vim.current.range.end + 2) + ')')) - offset - 2
+def main():
+  # Get the current text.
+  encoding = vim.eval("&encoding")
+  buf = get_buffer(encoding)
+  text = '\n'.join(buf)
 
-# Call formatter.
-p = subprocess.Popen([binary, '-offset', str(offset), '-length', str(length),
-                      '-style', style],
-                     stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                     stdin=subprocess.PIPE)
-stdout, stderr = p.communicate(input=text)
+  # Determine range to format.
+  if vim.eval('exists("l:lines")') == '1':
+    lines = vim.eval('l:lines')
+  else:
+    lines = '%s:%s' % (vim.current.range.start + 1, vim.current.range.end + 1)
 
-# If successful, replace buffer contents.
-if stderr:
-  message = stderr.splitlines()[0]
-  parts = message.split(' ', 2)
-  if len(parts) > 2:
-    message = parts[2]
-  print 'Formatting failed: %s (total %d warnings, %d errors)' % (
-      message, stderr.count('warning:'), stderr.count('error:'))
+  # Determine the cursor position.
+  cursor = int(vim.eval('line2byte(line("."))+col(".")')) - 2
+  if cursor < 0:
+    print('Couldn\'t determine cursor position. Is your file empty?')
+    return
 
-if not stdout:
-  print ('No output from clang-format (crashed?).\n' +
-      'Please report to bugs.llvm.org.')
-elif stdout != text:
-  lines = stdout.split('\n')
-  for i in range(min(len(buf), len(lines))):
-    buf[i] = lines[i]
-  for line in lines[len(buf):]:
-    buf.append(line)
-  del buf[len(lines):]
+  # Avoid flashing an ugly, ugly cmd prompt on Windows when invoking clang-format.
+  startupinfo = None
+  if sys.platform.startswith('win32'):
+    startupinfo = subprocess.STARTUPINFO()
+    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    startupinfo.wShowWindow = subprocess.SW_HIDE
+
+  # Call formatter.
+  command = [binary, '-style', style, '-cursor', str(cursor)]
+  if lines != 'all':
+    command.extend(['-lines', lines])
+  if fallback_style:
+    command.extend(['-fallback-style', fallback_style])
+  if vim.current.buffer.name:
+    command.extend(['-assume-filename', vim.current.buffer.name])
+  p = subprocess.Popen(command,
+                       stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                       stdin=subprocess.PIPE, startupinfo=startupinfo)
+  stdout, stderr = p.communicate(input=text.encode(encoding))
+
+  # If successful, replace buffer contents.
+  if stderr:
+    print(stderr)
+
+  if not stdout:
+    print(
+        'No output from clang-format (crashed?).\n'
+        'Please report to bugs.llvm.org.'
+    )
+  else:
+    lines = stdout.decode(encoding).split('\n')
+    output = json.loads(lines[0])
+    lines = lines[1:]
+    sequence = difflib.SequenceMatcher(None, buf, lines)
+    for op in reversed(sequence.get_opcodes()):
+      if op[0] is not 'equal':
+        vim.current.buffer[op[1]:op[2]] = lines[op[3]:op[4]]
+    if output.get('IncompleteFormat'):
+      print('clang-format: incomplete (syntax errors)')
+    vim.command('goto %d' % (output['Cursor'] + 1))
+
+main()

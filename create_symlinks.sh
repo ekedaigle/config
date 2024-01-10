179c962338972dac1b8ea3436235e3765c478b4a
Eric Kedaigle <ekedaigle@mitre.org>
2024-01-10 13:06:09 -0500

    Use clangd language server

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -9,7 +9,7 @@ ln -sfn "$base_dir/colorgcc/colorgcc.pl" ~/bin/c++
 ln -sfn "$base_dir/colorgcc/colorgcc.pl" ~/bin/cc
 ln -sfn "$base_dir/clang-format" ~/.clang-format
 ln -sfn "$base_dir/colorgcc/colorgcc" ~/.colorgccrc
-ln -sfn "$base_dir/flake8" $XDG_CONFIG_HOME/flake8
+ln -sfn "$base_dir/flake8" ~/.config/flake8
 ln -sfn "$base_dir/git/gitconfig" ~/.gitconfig
 ln -sfn "$base_dir/git/git_template" ~/.git_template
 ln -sfn "$base_dir/tmux/tmux.conf" ~/.tmux.conf
@@ -19,7 +19,8 @@ ln -sfn "$base_dir/tmux/tmux-session-created" ~/bin/tmux-session-created
 ln -sfn "$base_dir/tmux/tmux-session-closed" ~/bin/tmux-session-closed
 ln -sfn "$base_dir/vim/vimrc" ~/.vimrc
 ln -sfn "$base_dir/vim/vim" ~/.vim
-ln -sfn "$base_dir/vim/vimrc" ~/.nvimrc
+mkdir -p ~/.config/nvim
+ln -sfn "$base_dir/vim/vimrc" ~/.config/nvim/init.vim
 ln -sfn "$base_dir/vim/vim" ~/.nvim
 ln -sfn "$base_dir/zsh/oh-my-zsh" ~/.oh-my-zsh
 ln -sfn "$base_dir/zsh/zshrc" ~/.zshrc
---
+++ vim/vim/ftdetect/cmix.vim
@@ -0,0 +1,1 @@
+autocmd BufRead,BufNewFile *.dis set filetype=cmix 
